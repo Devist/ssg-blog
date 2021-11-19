a7300556a884695b16093f9e92d27f250b4cd9ff
OhDongHwan <Devist@users.noreply.github.com>
2021-11-19 15:16:30 +0900

    build(set-project): Add storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,24 @@ module.exports = {
   // https://tailwindcss.com/docs/just-in-time-mode
   // mode: "jit",
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {},
   plugins: [],
   purge: {
     // Filenames to scan for classes
     content: [
-      "./src/**/*.html",
-      "./src/**/*.js",
-      "./src/**/*.jsx",
-      "./src/**/*.ts",
-      "./src/**/*.tsx",
-      "./public/index.html",
+      './src/**/*.html',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './public/index.html'
     ],
     // Options passed to PurgeCSS
     options: {
       // Whitelist specific selectors by name
       // safelist: [],
-    },
-  },
-};
+    }
+  }
+}
